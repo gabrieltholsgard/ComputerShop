d005ef48773d74d556b7cab9e0f20a9bb583b448
staffanro <staffan.rodgren@hotmail.com>
2013-06-11 13:57:26 -0700

    mer fix

--- src/beans/BookListBean.java
+++ src/beans/BookListBean.java
@@ -24,21 +24,24 @@ public class BookListBean {
 	public BookListBean() {
 	}
 
-	public void refreshBean() throws Exception {
+	private void initBeans() throws Exception {
+		bookList = new ArrayList<BookBean>(); // a list
 		try {
-			bookList = new ArrayList<BookBean>();
+
 			// get a database connection and load the JDBC-driver
+
+			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(url);
 
 			// create SQL statements to load the books into the list
 			// each book is a BookBean object
+
 			stmt = conn.createStatement();
 			String sql = "SELECT B.BOOK_ID, B.TITLE, B.DESCRIPTION, AV.FINAL_PRICE AS PRICE, ";
-			sql += "B.PROFIT, B.VISIBLE, A.AUTHOR_ID, A.NAME, A.SURNAME, ";
-			sql += "A.QTY, A.C_PRICE, C.QTY AS NEEDED, AV.AVAILABILITY ";
-			sql += "FROM BOOKS AS B, AUTHORS AS A, COMPOSITION AS C, AVAILABILITY AS AV WHERE ";
-			sql += "AV.BOOK_ID = B.BOOK_ID AND B.BOOK_ID = C.EL_ID AND ";
-			sql += "A.AUTHOR_ID = C.COM_ID ORDER BY BOOK_ID";
+			sql += "B.PROFIT, B.VISIBLE, B.COM_ID, B.NAME, B.SURNAME, ";
+			sql += "B.IN_STOCK AS QTY, B.C_PRICE, B.QTY AS NEEDED, AV.AVAILABILITY ";
+			sql += "FROM NEEDED AS B, AVAILABILITY AS AV WHERE ";
+			sql += "AV.BOOK_ID = B.BOOK_ID ORDER BY BOOK_ID";
 			rs = stmt.executeQuery(sql);
 
 			// analyze the result set
@@ -50,11 +53,11 @@ public class BookListBean {
 					bid = rs.getInt("BOOK_ID");
 					bb.setId(rs.getInt("BOOK_ID"));
 					bb.setProduct(rs.getString("TITLE"));
-					bb.setManufacturer(rs.getInt("AUTHOR_ID"),
+					bb.setManufacturer(rs.getInt("COM_ID"),
 							rs.getString("NAME"));
-					bb.setType(rs.getInt("AUTHOR_ID"),
+					bb.setType(rs.getInt("COM_ID"),
 							rs.getString("SURNAME"));
-					bb.setComponentQuantity(rs.getInt("AUTHOR_ID"),
+					bb.setComponentQuantity(rs.getInt("COM_ID"),
 							rs.getInt("NEEDED"));
 					bb.setPrice(rs.getInt("PRICE"));
 					bb.setProfit(rs.getInt("PROFIT"));
@@ -63,11 +66,11 @@ public class BookListBean {
 					bb.setAvailability(rs.getInt("AVAILABILITY"));
 					bookList.add(bb);
 				} else {
-					bb.setManufacturer(rs.getInt("AUTHOR_ID"),
+					bb.setManufacturer(rs.getInt("COM_ID"),
 							rs.getString("NAME"));
-					bb.setType(rs.getInt("AUTHOR_ID"),
+					bb.setType(rs.getInt("COM_ID"),
 							rs.getString("SURNAME"));
-					bb.setComponentQuantity(rs.getInt("AUTHOR_ID"),
+					bb.setComponentQuantity(rs.getInt("COM_ID"),
 							rs.getInt("NEEDED"));
 				}
 			}
@@ -97,71 +100,9 @@ public class BookListBean {
 	 * Creates a new instance of BookListBean
 	 */
 	public BookListBean(String _url) throws Exception {
-		url = _url;
-		bookList = new ArrayList<BookBean>(); // a list
-		try {
-
-			// get a database connection and load the JDBC-driver
-
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(url);
-
-			// create SQL statements to load the books into the list
-			// each book is a BookBean object
-
-			stmt = conn.createStatement();
-			String sql = "SELECT BOOKS.BOOK_ID, BOOKS.TITLE, NAME AS AUTHOR_NAME, ";
-			sql += "SURNAME AS AUTHOR_SURNAME, COM_ID, ";
-			sql += "AVAILABILITY.FINAL_PRICE AS PRICE , DESCRIPTION, AVAILABILITY, VISIBLE, PROFIT FROM AVAILABILITY, BOOKS,";
-			sql += "AUTHORS, COMPOSITION WHERE AVAILABILITY.BOOK_ID = EL_ID AND AUTHORS.AUTHOR_ID = COM_ID AND BOOKS.BOOK_ID = EL_ID";
-			sql += " ORDER BY BOOK_ID";
-			rs = stmt.executeQuery(sql);
-
-			// analyze the result set
-			int bid = -1;
-			BookBean bb = null;
-			while (rs.next()) {
-				if (rs.getInt("BOOK_ID") != bid) {
-					bb = new BookBean();
-					bid = rs.getInt("BOOK_ID");
-					bb.setId(rs.getInt("BOOK_ID"));
-					bb.setProduct(rs.getString("TITLE"));
-					bb.setManufacturer(rs.getInt("COM_ID"),
-							rs.getString("AUTHOR_NAME"));
-					bb.setType(rs.getInt("COM_ID"),
-							rs.getString("AUTHOR_SURNAME"));
-					bb.setPrice(rs.getInt("PRICE"));
-					bb.setProfit(rs.getInt("PROFIT"));
-					bb.setDescription(rs.getString("DESCRIPTION"));
-					bb.setVisible(rs.getBoolean("VISIBLE"));
-					bb.setAvailability(rs.getInt("AVAILABILITY"));
-					bookList.add(bb);
-				} else {
-					bb.setManufacturer(rs.getInt("COM_ID"),
-							rs.getString("AUTHOR_NAME"));
-					bb.setType(rs.getInt("COM_ID"),
-							rs.getString("AUTHOR_SURNAME"));
-				}
-			}
-
-		} catch (SQLException sqle) {
-			throw new Exception(sqle);
-		} // note the we always try to close all services
-			// even if one or more fail to close
-		finally {
-			try {
-				rs.close();
-			} catch (Exception e) {
-			}
-			try {
-				stmt.close();
-			} catch (Exception e) {
-			}
-			try {
-				conn.close();
-			} catch (Exception e) {
-			}
-		}
+		this.url = _url;
+		initBeans();
+		
 	}
 
 	// return the booklist
@@ -171,7 +112,7 @@ public class BookListBean {
 
 	// create an XML document from the booklist
 	public String getXml() throws Exception {
-		refreshBean();
+		initBeans();
 		BookBean bb = null;
 		Iterator<BookBean> iter = bookList.iterator();
 		StringBuffer buff = new StringBuffer();
